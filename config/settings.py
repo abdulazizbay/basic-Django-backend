a75065d2d937ad44a9b98116a9467f0716c2ed45
Abdulaziz <abdulazizxalilov30@gmail.com>
2022-09-24 16:30:55 +0900

    heroku procfile

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=n0r)4go!p0tu138897x*lodu7q8z6yc)u$*i3xjl29ow%_ch0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com','localhost','127.0,0,1']
 
 
 # Application definition
